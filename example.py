3e61e464df9661f06a065bed99369fbf5640029b
Michail Nikolaev <michail.nikolaev@gmail.com>
2024-12-02 17:30:52 +0100

    Add example Python file

--- example.py
+++ example.py
@@ -1,1 +1,1 @@
-{"content": "def hello_world():\n    print('Hello from GitHub integration!')\n\nif __name__ == '__main__':\n    hello_world()", "encoding": "base64"}
+{"content": "def hello_world():\n    print('Hello from GitHub integration!')\n\nif __name__ == '__main__':\n    hello_world()", "encoding": "utf8"}
